b5ad11d1714ea07ffaae4e1d6e6d414b365039da
neha763 <nehaambekar1998@gmail.com>
2025-12-24 13:02:26 +0530

    added new shg service

---
+++ Shg-Service/src/main/java/com/missionShakti/ShgServiceApplication.java
@@ -0,0 +1,13 @@
+package com.missionShakti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShgServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShgServiceApplication.class, args);
+	}
+
+}
---
+++ Shg-Service/src/test/java/com/missionShakti/ShgServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.missionShakti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShgServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
